8baf9ceef77783a2c56c46f025aa53d7ecb16e0f
Leo007ua <leo.oliinyk@ukr.net>
2023-07-19 17:58:27 +0300

    add huke in code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { requestPhotos } from 'services/api';
+
 import { WraperStyled } from './WraperStyled';
 import SearchBar from './Searchbar/Searchbar';
 import Loader from './Loader/Loader';
@@ -7,88 +8,77 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
 import Modal from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    pictureName: '',
-    responcedPhotos: [],
-    isLoading: false,
-    error: null,
-    page: 1,
-    totalPictures: null,
-    modal: { isOpen: false, modalData: null },
-  };
+export default function App() {
+  const [pictureName, setPictureName] = useState('');
+  const [responcedPhotos, setResponcedPhotos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [totalPictures, setTotalPictures] = useState(null);
+  const [modal, setModal] = useState({
+    isOpen: false,
+    modalData: null,
+  });
 
-  async componentDidUpdate(_, prevState) {
-    const { pictureName, page } = this.state;
-    if (
-      this.state.pictureName !== prevState.pictureName ||
-      this.state.page !== prevState.page
-    ) {
+  useEffect(() => {
+    if (!pictureName) {
+      return;
+    }
+    async function fetchRender() {
       try {
-        this.setState({ isLoading: true });
+        setIsLoading(true);
         const responcedPhotos = await requestPhotos(pictureName, page);
-        // console.log(requestPhotos);
-        this.setState({
-          totalPictures: responcedPhotos.totalHits,
-          responcedPhotos:
-            page === 1
-              ? responcedPhotos.hits
-              : [...prevState.responcedPhotos, ...responcedPhotos.hits],
+        setTotalPictures(responcedPhotos.totalHits);
+        setResponcedPhotos(prevState => {
+          return page === 1
+            ? responcedPhotos.hits
+            : [...prevState, ...responcedPhotos.hits];
         });
       } catch (error) {
-        this.setState({ error: error.message });
+        setError(error.message);
       } finally {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
       }
     }
-  }
+    fetchRender();
+  }, [pictureName, page]);
 
-  fetchLoadMore = async () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const fetchLoadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  onSubmit = pictureName => {
-    this.setState({
-      pictureName: pictureName,
-      page: 1,
-    });
+  const onSubmit = pictureName => {
+    setPictureName(pictureName);
+    setPage(1);
   };
 
-  onOpenModal = data =>
-    this.setState({ modal: { isOpen: true, modalData: data } });
+  const onOpenModal = data => {
+    setModal({ isOpen: true, modalData: data });
+  };
 
-  onCloseModal = data =>
-    this.setState({ modal: { isOpen: false, modalData: null } });
+  const onCloseModal = () => setModal({ isOpen: false, modalData: null });
 
-  render() {
-    const {
-      responcedPhotos,
-      isLoading,
-      error,
-      modal: { isOpen, modalData },
-    } = this.state;
-    return (
-      <>
-        <WraperStyled>
-          <SearchBar onSubmit={this.onSubmit} />
-          {isLoading && <Loader />}
-          {error && <>Oops... Error: {error}</>}
-          {responcedPhotos?.length > 0 && (
-            <ImageGallery
-              responcedPhotos={responcedPhotos}
-              onOpenModal={this.onOpenModal}
-            />
+  return (
+    <>
+      <WraperStyled>
+        <SearchBar onSubmit={onSubmit} />
+        {isLoading && <Loader />}
+        {error && <>Oops... Error: {error}</>}
+        {responcedPhotos?.length > 0 && (
+          <ImageGallery
+            responcedPhotos={responcedPhotos}
+            onOpenModal={onOpenModal}
+          />
+        )}
+
+        {responcedPhotos.length > 0 &&
+          responcedPhotos.length < totalPictures && (
+            <Button fetchLoadMore={fetchLoadMore} />
           )}
-
-          {responcedPhotos.length > 0 &&
-            responcedPhotos.length < this.state.totalPictures && (
-              <Button fetchLoadMore={this.fetchLoadMore} />
-            )}
-          {isOpen && (
-            <Modal onCloseModal={this.onCloseModal} modalData={modalData} />
-          )}
-        </WraperStyled>
-      </>
-    );
-  }
+        {modal.isOpen && (
+          <Modal onCloseModal={onCloseModal} modalData={modal.modalData} />
+        )}
+      </WraperStyled>
+    </>
+  );
 }
