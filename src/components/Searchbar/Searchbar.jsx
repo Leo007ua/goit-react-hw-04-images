8b7c03d4950b29a57d289906669c1e4a2bcacd88
Leo007ua <leo.oliinyk@ukr.net>
2023-07-20 21:19:47 +0300

    add huke searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,49 +1,38 @@
 import PropTypes from 'prop-types'
-import { Component } from 'react';
+import { useState } from 'react';
 import { SearchbarStyled } from './SearchbarStyled';
 
-class SearchBar extends Component {
-  state = {
-    pictureName: '',
-  };
+function SearchBar ({onSubmit}) {
+const [pictureName, setPictureName] = useState('');
 
-  handleOnChange = evt => {
-    this.setState({
-      pictureName: evt.target.value,
-    });
-  };
-  formOnSubmit = evt => {
+const formOnSubmit = evt => {
     evt.preventDefault();
-    this.props.onSubmit(this.state.pictureName);
-    this.resetForm(evt);
-  }
+    onSubmit(pictureName);
+    setPictureName('');
+  };
 
-  resetForm = evt => {
-    evt.target.reset();
-    this.setState({pictureName: ''})
-  }
-  render() {
     return (
       <SearchbarStyled className="searchbar">
-        <form className="form" onSubmit={this.formOnSubmit}>
+        <form className="form" onSubmit={formOnSubmit}>
           <button type="submit" className="button">
             <span className="button-label">Search</span>
           </button>
 
           <input
+          className="input"
             name="pictureName"
-            className="input"
+           value={pictureName}
             type="text"
             autoComplete="off"
             autoFocus
             placeholder="Search images and photos"
-            onChange = {this.handleOnChange}
+            onChange = {evt => setPictureName(evt.target.value)}
           />
         </form>
       </SearchbarStyled>
     );
   }
-}
+
 
 SearchBar.propsTypes = {
   onSubmit: PropTypes.func.isRequired,
